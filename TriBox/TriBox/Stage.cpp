26ed18c4ba356868e34295e7ba898f7758683dac
SyDs <setuclub@gmail.com>
2018-10-30 02:04:34 +0900

    stage行列変換　バグ

--- TriBox/TriBox/Stage.cpp
+++ TriBox/TriBox/Stage.cpp
@@ -2,6 +2,7 @@
 //#include "DxLib.h"
 #include <stdio.h>
 #include "Block.h"
+#include <iostream>
 
 Stage::Stage()
 {
@@ -16,29 +17,54 @@ Stage::~Stage()
 void Stage::LoadStageData()
 {
 	FILE* fp = fopen("Resource/map/TriBox仮配置データ.fmf", "rb");
+
+	std::vector<unsigned char> dummyData;
+
 	fread(&stagedata, sizeof(StageData), 1, fp);
-	auto layersize = stagedata.mapwidth * stagedata.mapheight;
-	//stageArrangement.resize(layersize);
-	for (int i = 0; i < layersize; ++i)
+
+	auto layersize = stagedata.mapwidth * stagedata.mapheight * (stagedata.bitcount / 8);
+
+	dummyData.resize(layersize);
+	stageArrangement.resize(dummyData.size());
+
+	for (auto& stage : dummyData)
 	{
-		//fread(&stageArrangement[i], layersize, 1, fp);
+		fread(&stage, sizeof(unsigned char), 1, fp);
 	}
+	
 	fclose(fp);
 
-
-	int mapy = 0;
-	for (int i = 0; i < layersize; ++i)
+	for (int x = 0; x < stagedata.mapwidth; /*++x*/)
 	{
-		if (i % 60 == 0)
+		for (int y = 0; y < stagedata.mapheight; ++y)
 		{
-			//ステージ端まで行ったら+1
-			mapy += 1;
+			if (y % stagedata.mapheight == 0)
+			{
+				//stageArrangement[y * stagedata.mapheight + x];
+				stageArrangement[y * stagedata.mapheight + x] = dummyData[x * stagedata.mapwidth + y];
+			}
 		}
-		//if (stageArrangement[i] == 1)
-		{
-			//block.push_back(Block(Position2f(50 * i, 50 * mapy )));
-		}
+		++x;
 	}
+
+	//ダミーデータの解放
+	dummyData.clear();
+
+	int a = 0;
+
+	//int mapy = 0;
+	//for (int i = 0; i < layersize; ++i)
+	//{
+	//	if (i % 60 == 0)
+	//	{
+	//		//ステージ端まで行ったら+1
+	//		mapy += 1;
+	//	}
+	//	//if (stageArrangement[i] == 1)
+	//	{
+	//		//block.push_back(Block(Position2f(50 * i, 50 * mapy )));
+	//	}
+	//}
 }
 
 //std::vector<unsigned char> Stage::GetStageData()const
@@ -46,10 +72,10 @@ void Stage::LoadStageData()
 //	//return stageArrangement;
 //}
 
-unsigned int Stage::GetStageWidth()const
-{
-	return stagedata.mapwidth;
-}
+//unsigned int Stage::GetStageWidth()const
+//{
+//	return stagedata.mapwidth;
+//}
 
 void Stage::Draw()
 {
